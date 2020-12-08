feb038be9ee6dba4c54c785d634be4a0d47ed3ac
Jérémy <3DX900@gmail.com>
2020-12-08 20:17:33 +0100

    fix route path

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router(),
     passport = require('passport');
 
-router.post('users/login', (req, res, next) => {
+router.post('/users/login', (req, res, next) => {
     passport.authenticate('local', { session: false }, (err, user, info) => {
         if (err) return next(err);
 
